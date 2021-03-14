f227ec9e4356ee18f695246c8e217dd78a566845
Zack Hersh <zackhersh@Zacks-MBP.attlocal.net>
2021-03-13 21:04:11 -0500

    js for displaying results

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 var form = $('form')
 var input = $('input')
 var formBtn = $("#formBtn")
+var currentWeather =  $("#currentWeatherCont")
 
 form.on("submit", getApi)
 
@@ -11,7 +12,7 @@ function getApi(event){
     event.preventDefault()
 
 
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=+" + input[0].value + "&appid=4cfa8391d48a0b8394a66f203037d51e"
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=+" + input[0].value + "&appid=4cfa8391d48a0b8394a66f203037d51e&units=imperial"
     console.log(requestUrl)
     fetch(requestUrl)
         .then(function(response) {
@@ -19,7 +20,22 @@ function getApi(event){
         }) 
         .then(function(data) {
             console.log(data)
-            
+
+            var city = $("<h3>")
+            city.text(input[0].value)
+            currentWeather.append(city)
+
+            var temp = $("<p>")
+            temp.text("Temperature: " + data.main.temp + "°F")
+            currentWeather.append(temp)
+
+            var humid = $("<p>")
+            humid.text("Humidity: " + data.main.humidity + "%")
+            currentWeather.append(humid)
+
+            var wind = $("<p>")
+            wind.text("Wind Speed: " + data.wind.speed + " MPH")
+            currentWeather.append(wind)
         });
 }
 
